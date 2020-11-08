46628b5d6d18919a7ec5b8d46494b6b22187446d
Nouu <noelarrieulacoste@yahoo.fr>
2020-11-08 17:53:32 +0100

    cross compile

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include "gtk_functions.h"
 
 int main(int argc, char **argv) {
-    char *gladeFilePath = strdup("gui_files/gui.glade");
+#ifdef __unix__
+    setenv("DISPLAY", "127.0.0.1:0", 1);
+#endif
+    char *gladeFilePath = strdup("./gui_files/gui.glade");
 
     startGTK(&argc, &argv, gladeFilePath);
 
